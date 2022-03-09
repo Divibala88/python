e336a0ab475fb6a2e79b425c5620caee57c59e4b
Divibala88 <101201403+Divibala88@users.noreply.github.com>
2022-03-09 11:28:07 +0400

    Add files via upload

---
+++ createlogin.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("login.html")
+
+@app.route('/new')
+def new_user():
+    return render_template("register.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
